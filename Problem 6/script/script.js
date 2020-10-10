b909a5c5e35d9fec531772056f6fe3488f315b4f
Prajyotmane <prajyotmane464@gmail.com>
2020-10-10 11:53:07 -0700

    Problem 6 commit

--- Problem 6/script/script.js
+++ Problem 6/script/script.js
@@ -1,27 +1,7 @@
-var num1;
-var num2;
-
-num1 = window.prompt("Enter the first number");
-num1 = parseInt(num1);
-
-num2 = window.prompt("Enter the second number");
-num2 = parseInt(num2);
-
-if(isNaN(num1)){
-    document.write("First number you entered was not a valid number");
-}else if(isNaN(num2)){
-    document.write("Second number you entered was not a valid number");
-}
-else if(num1>num2){
-    document.write("First number was : " + num1.toString() + "<br>");
-    document.write("Second number was: " + num2.toString() + "<br>");
-    document.write("Large number was: " + num1.toString() + "<br>");
-}else if(num1<num2){
-    document.write("First number was : " + num1.toString() + "<br>");
-    document.write("Second number was: " + num2.toString() + "<br>");
-    document.write("Large number was: " + num2.toString() + "<br>");
-}else{
-    document.write("First number was : " + num1.toString() + "<br>");
-    document.write("Second number was: " + num2.toString() + "<br>");
-    document.write("Both numbers are equal" + "<br>");
+for(var num = 0; num<16; num++){
+    if(num%2==0){
+        document.write(num.toString() + " is even" + "<br>");
+    }else{ 
+        document.write(num.toString() + " is odd" + "<br>");
+    }
 }
